155a2fe021fd198f15f99c0f3c0fa395b84ea381
Juan Sasia <juanignaciosasia@gmail.com>
2025-03-26 18:10:20 -0300

    primeros dos puntos resultos Agregar archivos de proyecto.

---
+++ Sasia1Colombara2/Program.cs
@@ -0,0 +1,46 @@
+﻿using Sasia1Colombara2.Models;
+
+namespace Sasia1Colombara2 {
+
+    public class program
+    {
+
+        /*
+         * Alumnos:
+         * Sasia Juan Ignacio, Dni 37713879
+         * Colombara Francisco, Dni
+         * Re Florencia, Dni
+         * Cintia, 
+         */
+        static void Main(string[] args)
+        {
+
+            Alumno nuevoAlumno = new Alumno();
+
+                Console.WriteLine("Ingrese los datos del Usuario: ");
+                
+                Console.Write("Usuario: ");
+                nuevoAlumno.usuario = Convert.ToInt32(Console.ReadLine());
+
+            Console.Write("Apellido: ");
+                nuevoAlumno.apellido = Console.ReadLine();
+
+                Console.Write("Nombre: ");
+                nuevoAlumno.nombre = Console.ReadLine();
+
+                Console.Write("Celular: ");
+                nuevoAlumno.celular = Console.ReadLine();
+
+                Console.WriteLine("--------------------------------");
+
+
+            Console.WriteLine("La persona: " + nuevoAlumno.apellido
+                + ", " + nuevoAlumno.nombre
+                + " tiene el usuiario: " + nuevoAlumno.usuario
+                + " y el celular: " + nuevoAlumno.celular);
+            }
+
+
+        }
+
+    }
